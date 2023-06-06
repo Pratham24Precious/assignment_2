29ebf9eb89bb10245695ea54d442b5d523468711
Pratham24Precious <134937806+Pratham24Precious@users.noreply.github.com>
2023-06-06 17:28:12 +0530

    Update pr_1.rb

--- pr_1.rb
+++ pr_1.rb
@@ -1,3 +1,4 @@
+# Implement a mixin module that provides a greet method. Create two classes, Person and Robot, and include the mixin module in both classes. The greet method should output a greeting message specific to each class.
 module Mixin
     def self.greet(name)
         puts "Hii #{name}, Greetings of the day!"
